c796847966180f42918e00bbf2e0005ee4138a38
fridayshoes <one_amp@hotmail.com>
2022-05-19 17:19:56 +0100

    Testing text for presence of #TODO

--- lib/check_for_todo.rb
+++ lib/check_for_todo.rb
@@ -1,3 +1,7 @@
 def check_for_todo(text)
   fail "There's no text to check." if text.empty?
+  if text.include? "#TODO"
+    return true
+  else
+  end
 end
--- spec/check_for_todo_spec.rb
+++ spec/check_for_todo_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe "check_for_todo" do
       expect { check_for_todo("")}.to raise_error "There's no text to check."
     end
   end
+
+  context "when a string starts with the text #TODO" do
+    it "returns true" do
+      expect(check_for_todo("#TODO wash the car")).to eq true
+    end
+  end
+
+  context "when a string contains the #TODO anywhere in the text" do
+    it "returns true" do
+      expect(check_for_todo("There's so many things #TODO today")).to eq true
+    end
+  end
 end
