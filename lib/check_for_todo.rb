9d094546ff1487cb51dd3a0b10c5a222d92cb49b
Jennie Evans <jennie1evans@hotmail.com>
2022-05-24 09:27:04 +0100

    Refactored check_for_todo

--- lib/check_for_todo.rb
+++ lib/check_for_todo.rb
@@ -1,8 +1,11 @@
 def check_for_todo(text)
   fail "There's no text to check." if text.empty?
-  if text.include? "#TODO"
-    return true
-  else
-    return false
-  end
+  text.include? "#TODO"  
 end
+
+#code below was refactored to the above
+#if text.include? "#TODO"
+ # return true
+#else
+ # return false
+#end
